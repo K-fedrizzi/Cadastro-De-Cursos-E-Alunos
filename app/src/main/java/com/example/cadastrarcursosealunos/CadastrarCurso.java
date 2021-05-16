cf0de7c31ae72e15ba6d2db4940ec53a0867f4db
Emerson Jose da Silva Costa <emerson.costa2785@gmail.com>
2021-05-15 22:01:57 -0300

    Ligação e alguns métodos criados.
    Criação dos métodos referentes aos botões.

--- app/src/main/java/com/example/cadastrarcursosealunos/CadastrarCurso.java
+++ app/src/main/java/com/example/cadastrarcursosealunos/CadastrarCurso.java
@@ -2,7 +2,9 @@ package com.example.cadastrarcursosealunos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,5 +22,13 @@ public class CadastrarCurso extends AppCompatActivity {
         edtCadHoras = findViewById(R.id.edtCadHoras);
         btnCadCadastrarCurso = findViewById(R.id.btnCadCadastrarCurso);
         btnCadVoltar = findViewById(R.id.btnCadVoltar);
+    }
+
+    public void cadastrarCurso(View view) {
+    }
+
+    public void voltar(View view) {
+        Intent it=new Intent(this,MenuActivity.class);
+        startActivity(it);
     }
 }
